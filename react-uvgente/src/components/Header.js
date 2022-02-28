8c764bf06fc1d5a1cc7d5f28501ce264f184a98c
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-02-27 18:23:25 -0600

    Create-app react and header component
    Comando para iniciar:
    npm start
    Cambiar de directorio para que funcione

---
+++ react-uvgente/src/components/Header.js
@@ -0,0 +1,30 @@
+import logo from '../media/UVGente_Logo_white.png' 
+import signo_mas_img from '../media/signo_mas_blanco.png' 
+import NavigationElements from './NavigationElements'
+const Header = () => {
+  return (
+    <header className='header'>
+
+        <img src={logo} alt='Logo UVGente' className='logo'/>
+
+        {/*Este componente es temporal, en lo que averiguo como pasar direccion de imagen como paramentro al cmpnente */}
+        <div >
+            <h3> Vender 
+            <img src={signo_mas_img} alt='Mas' className='logo'></img>
+            </h3>
+        </div>
+        
+        <NavigationElements title='Top Ventas'/>
+        <NavigationElements title='Top Servicios'/>
+        <NavigationElements title='Productos'/>
+        <NavigationElements title='Servicios' last_element = "True"/><div></div>
+
+
+
+
+   
+    </header>
+  )
+}
+
+export default Header
---
+++ react-uvgente/src/components/NavigationElements.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types'
+
+const NavigationElements = (props) => {
+    const title = props.title
+    const image = props.image
+    const last_element = props.last_element
+
+    if(image=== 'none')
+    {
+        return (
+            <div>
+                <h3> {title} </h3>
+            </div>
+        )
+    }
+    else 
+    {
+        return(
+            <div>
+                <p>{image}</p>
+                <h3> {title} </h3>
+            </div>
+        )
+        
+    }
+
+}
+
+NavigationElements.defaultProps = {
+    title: 'NO-NAME',
+    imagen: 'none',
+    last_element: 'False'
+}
+
+export default NavigationElements
