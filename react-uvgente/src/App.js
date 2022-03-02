2c0be0032daddcdb1b710ef1f6065975c368e632
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-03-02 14:53:43 -0600

    Componente comun Barra lateral

--- react-uvgente/src/App.js
+++ react-uvgente/src/App.js
@@ -1,10 +1,15 @@
+import BarraLateral from "./components/BarraLateral";
 import Header from "./components/Header";
+import MiPerfil from "./components/MiPerfil";
 
 function App() {
   return (
     <div className="container">
       <Header navSet="ON"/>
-      
+      <div className="flex_container_sidebar_and_content">
+        <BarraLateral />
+      </div>
+  
     </div>
   );
 }
