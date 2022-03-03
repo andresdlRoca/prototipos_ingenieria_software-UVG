4ac9bfc83999ec58210ff0fb9cc7fa9bc72d8ee8
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-03-02 18:00:02 -0600

    Update App.js

--- react-uvgente/src/App.js
+++ react-uvgente/src/App.js
@@ -1,11 +1,11 @@
 import BarraLateral from "./components/BarraLateral";
 import Header from "./components/Header";
-import MiPerfil from "./components/MiPerfil";
+//import MiPerfil from "./components/MiPerfil";
 
 function App() {
   return (
     <div className="container">
-      <Header navSet="ON"/>
+      <Header navSet="OFF"/>{/** navSet = "ON" para mostrar opcinones en la barra de navegacion; OFF para no mostroarla */}
       <div className="flex_container_sidebar_and_content">
         <BarraLateral />
       </div>
