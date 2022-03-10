0d24d11a922ecb1dfbc7b8ba60ac9482c601766e
jwlh00 <juunshuu00@gmail.com>
2022-03-10 12:47:49 -0600

    Agregar vista de reportar problema

---
+++ react-uvgente/src/App.js
@@ -0,0 +1,17 @@
+import BarraLateral from "./components/BarraLateral";
+import Header from "./components/Header";
+import Reportar_Problema from "./components/Reportar_Problema"
+
+function App() {
+  return (
+    <div className="container">
+      <Header navSet="ON"/>
+      <div className="flex_container_sidebar_and_content">
+        <BarraLateral />
+        <Reportar_Problema />
+      </div>
+  
+    </div>
+  );
+}
+export default App;
