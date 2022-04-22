c51034821d44443c981bb47fde873c63a470ef98
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-04-22 00:31:48 -0600

    Actualizacion de uso de rutas

--- react-uvgente/src/App.js
+++ react-uvgente/src/App.js
@@ -3,8 +3,6 @@ import BuscadorMain from "./components/BuscadorMain";
 import Header from "./components/Header";
 import Bandeja_Entrada from "./components/Bandeja_Entrada/Bandeja_Entrada";
 import Reportar_Problema from "./components/Reportar_Problema"
-import TutoriaInfo from "./components/TutoriaInfo";
-import VentaInfo from "./components/VentaInfo";
 import Singup from "./components/Signup/Singup";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import MiPerfil from  "./components/MiPerfil"
@@ -14,36 +12,55 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Router>
-      <div >
-        <Header navSet="OFF"/>{/** navSet = "ON" para mostrar opcinones en la barra de navegacion; OFF para no mostroarla */}          
+                  
           <Routes>
           <Route path="/singup" element = {
+            <div>
+              <Header navSet="OFF"/>
               <Singup />
-            }/>
-            <Route path="/" element = {
-            <div className="flex_container_sidebar_and_content">
-              <BarraLateral /> 
-              <BuscadorMain />
             </div>
             }/>
+            <Route path="/login" element = {
+              <div>
+                <Header navSet="OFF"/>
+                <Login />
+              </div>
+            }/>
+            <Route path="/" element = {
+              <div>
+                <Header navSet="ON"/>
+                <div className="flex_container_sidebar_and_content">
+                  <BarraLateral /> 
+                  <BuscadorMain />
+                </div>
+              </div>
+            }/>
             <Route path="/profile" element = {
-              <div className="flex_container_sidebar_and_content">
-                <BarraLateral /><MiPerfil />
+               <div>
+                <Header navSet="ON"/>
+                <div className="flex_container_sidebar_and_content">
+                  <BarraLateral /><MiPerfil />
+                </div>
               </div>
             }/>
             <Route path="/report" element = {
-              <div className="flex_container_sidebar_and_content">
-                <BarraLateral /><Reportar_Problema />
+               <div>
+                <Header navSet="ON"/>
+                <div className="flex_container_sidebar_and_content">
+                  <BarraLateral /><Reportar_Problema />
+                </div>
               </div>
             }/>
             <Route path="/bandeja-de-entrada" element = {
-              <div className="flex_container_sidebar_and_content">
-                <BarraLateral />
-                <Bandeja_Entrada />
+               <div>
+                <Header navSet="ON"/>
+                <div className="flex_container_sidebar_and_content">
+                    <BarraLateral />
+                    <Bandeja_Entrada />
+                </div>
               </div>
             }/>
           </Routes>
-      </div>
     </Router>
   );
 }
