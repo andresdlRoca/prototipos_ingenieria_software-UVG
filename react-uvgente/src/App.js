75db97a865d40e40eeb269ac41a76a693ed40f1d
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-04-22 14:16:39 -0600

    change on signup route

--- react-uvgente/src/App.js
+++ react-uvgente/src/App.js
@@ -14,7 +14,7 @@ function App() {
     <Router>
                   
           <Routes>
-          <Route path="/singup" element = {
+          <Route path="/signup" element = {
             <div>
               <Header navSet="OFF"/>
               <Singup />
