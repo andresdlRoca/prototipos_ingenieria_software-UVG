6530afc30414a61a54565d8a6a13f3d2325643cb
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-04-22 00:17:02 -0600

    update on use of routes

--- react-uvgente/src/App.js
+++ react-uvgente/src/App.js
@@ -5,25 +5,44 @@ import Bandeja_Entrada from "./components/Bandeja_Entrada/Bandeja_Entrada";
 import Reportar_Problema from "./components/Reportar_Problema"
 import TutoriaInfo from "./components/TutoriaInfo";
 import VentaInfo from "./components/VentaInfo";
-import Singup from "./components/Singup";
+import Singup from "./components/Signup/Singup";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import MiPerfil from  "./components/MiPerfil"
 import Login from "./components/Login"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <Router>
-      <div className="container">
-        <Header navSet="OFF"/>{/** navSet = "ON" para mostrar opcinones en la barra de navegacion; OFF para no mostroarla */}
-        <div className="flex_container_sidebar_and_content">
-          <BarraLateral />
+      <div >
+        <Header navSet="OFF"/>{/** navSet = "ON" para mostrar opcinones en la barra de navegacion; OFF para no mostroarla */}          
           <Routes>
-            <Route path="/" element = {<BuscadorMain />}/>
-            <Route path="/profile" element = {<MiPerfil />}/>
-            <Route path="/report" element = {<Reportar_Problema />}/>
-            <Route path="/bandeja-de-entrada" element = {<Bandeja_Entrada />}/>
-           
+          <Route path="/singup" element = {
+              <Singup />
+            }/>
+            <Route path="/" element = {
+            <div className="flex_container_sidebar_and_content">
+              <BarraLateral /> 
+              <BuscadorMain />
+            </div>
+            }/>
+            <Route path="/profile" element = {
+              <div className="flex_container_sidebar_and_content">
+                <BarraLateral /><MiPerfil />
+              </div>
+            }/>
+            <Route path="/report" element = {
+              <div className="flex_container_sidebar_and_content">
+                <BarraLateral /><Reportar_Problema />
+              </div>
+            }/>
+            <Route path="/bandeja-de-entrada" element = {
+              <div className="flex_container_sidebar_and_content">
+                <BarraLateral />
+                <Bandeja_Entrada />
+              </div>
+            }/>
           </Routes>
-        </div>
       </div>
     </Router>
   );
