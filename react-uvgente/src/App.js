97561d9dc637095f18c425274dbf6ad82212aee4
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2022-04-29 19:17:12 -0600

    Update App.js

--- react-uvgente/src/App.js
+++ react-uvgente/src/App.js
@@ -1,16 +1,88 @@
-import "./index.css";
-import Header from "./components/Header";
 import BarraLateral from "./components/BarraLateral";
+import BuscadorMain from "./components/BuscadorMain";
+import Header from "./components/Header";
+import Bandeja_Entrada from "./components/Bandeja_Entrada/Bandeja_Entrada";
 import Reportar_Problema from "./components/Reportar_Problema";
+import Singup from "./components/Signup/Singup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MiPerfil from "./components/MiPerfil";
+import Login from "./components/Login";
+import Reportar_Problema from "./components/Reportar_Problema";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function App() {
+function App() {
   return (
-    <div className="container">
-      <Header navSet="ON" />
-      <div className="flex_container_sidebar_and_content">
-        <BarraLateral />
-        <Reportar_Problema />
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/signup"
+          element={
+            <div>
+              <Header navSet="ON" />
+              <Singup />
+            </div>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <div>
+              <Header navSet="ON" />
+              <Login />
+            </div>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <div>
+              <Header navSet="ON" />
+              <div className="flex_container_sidebar_and_content">
+                <BarraLateral />
+                <BuscadorMain />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <div>
+              <Header navSet="ON" />
+              <div className="flex_container_sidebar_and_content">
+                <BarraLateral />
+                <MiPerfil />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/report"
+          element={
+            <div>
+              <Header navSet="ON" />
+              <div className="flex_container_sidebar_and_content">
+                <BarraLateral />
+                <Reportar_Problema />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/bandeja-de-entrada"
+          element={
+            <div>
+              <Header navSet="ON" />
+              <div className="flex_container_sidebar_and_content">
+                <BarraLateral />
+                <Bandeja_Entrada />
+              </div>
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
