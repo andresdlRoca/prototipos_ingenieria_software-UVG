44920c6e5f2d28f061808978cb7d0779cb5e3981
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-05-05 13:23:18 -0600

    adapt pagina principal into app.js

--- react-uvgente/src/App.js
+++ react-uvgente/src/App.js
@@ -7,13 +7,14 @@ import Reportar_Problema from "./components/Reportar_Problema";
 import Singup from "./components/Signup/Singup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MiPerfil from "./components/MiPerfil";
-import Login from "./components/Login";
+import Login from "./components/Login/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Ayuda from "./components/Ayuda/Ayuda";
 import NavBar from "./components/new-nav-bar/NavBar";
 import NewSideBar from "./components/new-side-bar/NewSideBar";
 import NewVentas from "./components/Ventas/NewVentas";
 import NewServicio from "./components/Servicio/NewServicio";
+import NewPaginaPrincipal from "./components/NewPaginaPrincipal/NewPaginaPrincipal";
 function App() {
   return (
     <Router >
@@ -52,7 +53,7 @@ function App() {
           element={
             <div className="all-wrapper">
               <NavBar />
-                <BuscadorMain/>
+                <NewPaginaPrincipal/>
                 <NewSideBar/>
            
             </div>
