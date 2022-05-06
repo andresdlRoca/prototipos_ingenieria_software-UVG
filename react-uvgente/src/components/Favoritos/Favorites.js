a02d26e557c446788ce802fd9906f4518ebf5c1a
jwlh00 <juunshuu00@gmail.com>
2022-05-06 01:32:17 -0600

    creating favorites

---
+++ react-uvgente/src/components/Favoritos/Favorites.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TutoriaInfo from "./TutoriaInfo";
+import VentaInfo from "./VentaInfo";
+
+import "./Favorites.css";
+
+function Favorites() {
+    return(
+        <div className="mainContainer">
+            
+        </div>
+    )
+}
+
+export default Favorites;
