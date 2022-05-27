b2f168d236c82bf5fbcb36c5f7954eba67473806
andresdlRoca <andresdelaroca11@gmail.com>
2022-05-27 13:35:11 -0600

    Avances perfil de usuario - Rediseño

--- react-uvgente/src/components/MiPerfil.js
+++ react-uvgente/src/components/MiPerfil.js
@@ -6,8 +6,22 @@ const MiPerfil = () => {
     <div className='container_main_content' id='profile_page_main'>
         <div id='lateral_profile_page_main'>
             <img src={profile_pic} alt="profilepic" className='pp_into_side_bar_card_main_content' />
-            <p>Valoracion</p>
-            <img src={five_stars_image} alt="profilepic" className='five_start_into_main_content' />
+            <div className='valoracion-container'>
+                <p className="titleValoracion" >Valoracion</p>
+                <img src={five_stars_image} alt="profilepic" className='five_start_into_main_content' />
+            </div>
+        </div>
+        <div id='main_content_profile_page_main'>
+            <h4 className='userProfileInfo'>
+            Geralt Hernandez <br />
+            Estudiante UVG <br />
+            Ingeniería en Ciencias de la Computacion y TI <br/>
+            <br /> Sobre mi...
+            <br />
+            </h4>
+            <p className='userProfileDesc'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet tellus eleifend, rutrum purus vel, faucibus tellus. Maecenas efficitur lacus a aliquet scelerisque. Aenean vitae tincidunt magna.
+            </p>
             <div id='text_into_bar'>
                 <p>
                             Contactos: <br /><br />
@@ -17,21 +31,6 @@ const MiPerfil = () => {
                             <a href='https://www.linkedin.com/in/luis-pedro-gonzalez-aldana-3462311ab'>Linkedin</a> <br />
                             <a href='https://github.com/LPELCRACK896'>Github</a>
                 </p>
-            </div>
-        </div>
-        <div id='main_content_profile_page_main'>
-            <h4>
-            Geralt Hernandez <br />
-            Estudiante UVG <br />
-            Ingeniería en Ciencias de la Computacion y TI <br/>
-            <br /> Sobre mi...
-            <br />
-            </h4>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet tellus eleifend, rutrum purus vel, faucibus tellus. Maecenas efficitur lacus a aliquet scelerisque. Aenean vitae tincidunt magna.
-            </p>
-            <div id='btn_div'>
-                <button name="button" id='btn'></button>        
             </div>
         </div>
     </div>
