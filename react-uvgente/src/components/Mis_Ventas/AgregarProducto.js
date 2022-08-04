5d105aea1ed286e3578d5c1beae1eb481a50d37f
andresdlRoca <andresdelaroca11@gmail.com>
2022-08-03 18:36:23 -0600

    Funcionalidad de boton de producto y formulario inicial

--- react-uvgente/src/components/Mis_Ventas/AgregarProducto.js
+++ react-uvgente/src/components/Mis_Ventas/AgregarProducto.js
@@ -1,10 +1,53 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import "./Cards_Ventas.css"
 
 const AgregarProducto = (props) => {
 
+    const [productInfo, sumbitProductInfo] = useState({
+        nombre: '',
+        precio: '',
+        categoria: '',
+        descripcion: ''
+    })
+
+    const handleChange = (event) => {
+        sumbitProductInfo({
+            ...productInfo,
+            [event.target.name] : event.target.value
+        })
+    }
+
+    const handleSumbit = (event) => {
+        event.preventDefault()
+        console.log("Datos: "+ productInfo.nombre)
+    }
+
+
     return(
-        <div>Pass</div>
+        <Fragment>
+            <h1>Vender producto</h1>
+            <form className='formProducto' onSubmit={handleSumbit}>
+                <div className='pregunta'>
+                    <input type="text" placeholder="Nombre del producto" className="formInput" onChange={handleChange} name="nombre"></input>
+                </div>
+                <div className='pregunta'>
+                    <input type="text" placeholder="Precio del producto" className="formInput" onChange={handleChange} name="precio"></input>
+                </div>
+                <div className='pregunta'>
+                    <input type="text" placeholder="Categoria" className="formInput" onChange={handleChange} name="categoria"></input>
+                </div>
+                <div className='pregunta'>
+                    <input type="text" placeholder="Descripcion" className="formInput" onChange={handleChange} name="descripcion"></input>
+                </div>
+                <button type="sumbit" className='buttonSumbit'>Continuar</button>
+            </form>
+            <ul>
+                <li>{productInfo.nombre}</li>
+                <li>{productInfo.precio}</li>
+                <li>{productInfo.categoria}</li>
+                <li>{productInfo.descripcion}</li>
+            </ul>
+        </Fragment>
     )
 
 }
--- react-uvgente/src/components/Mis_Ventas/FloatingAgregar.js
+++ react-uvgente/src/components/Mis_Ventas/FloatingAgregar.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import "./Cards_Ventas.css"
 import addproduct from "../../media/add.png";
-
+import { Link } from 'react-router-dom'
 const FloatingAgregar = (props) => {
-
     return(
-        <div className="addproduct">
-            <img src={addproduct} alt="addproduct ic" />
+        <div className="addproduct" onClick={() => console.log("Hola")}>
+            <Link className="Link-to-publicar-articulo" to="/publicar-articulos">
+                <img src={addproduct} alt="addproduct ic" />
+            </Link>
         </div>
     )
 }
