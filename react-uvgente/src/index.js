19e788c141977398f8b60599df2882ff204ffb0d
robertriosm <87707371+robertriosm@users.noreply.github.com>
2022-08-17 18:53:13 -0600

    avances en formularios de registro
    + se agrego react-bootstrap

--- react-uvgente/src/index.js
+++ react-uvgente/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
