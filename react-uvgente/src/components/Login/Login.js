5c2d3e6c92125b68bdcbe41476ba581490c337d2
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2022-08-31 18:21:42 -0600

    Creación de las Vistas Privadas

--- react-uvgente/src/components/Login/Login.js
+++ react-uvgente/src/components/Login/Login.js
@@ -1,75 +1,77 @@
-import { Component } from 'react';
-import './Login_style.css';
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
-import { Link } from 'react-router-dom';
+import { Component } from "react"
+import "./Login_style.css"
+import Swal from "sweetalert2"
+import withReactContent from "sweetalert2-react-content"
+import { Link } from "react-router-dom"
+import PrivateRoute from "../PrivateRoute"
 
-const MySwal = withReactContent(Swal);
+const MySwal = withReactContent(Swal)
 
 class Login extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
-      email: '',
-      password: '',
-    };
-    this.changeEmail = this.changeEmail.bind(this);
-    this.changePassword = this.changePassword.bind(this);
-    this.enviar = this.enviar.bind(this);
+      email: "",
+      password: "",
+    }
+    this.changeEmail = this.changeEmail.bind(this)
+    this.changePassword = this.changePassword.bind(this)
+    this.enviar = this.enviar.bind(this)
   }
 
   changeEmail($event) {
-    const newValue = $event.target.value;
-    this.setState({ email: newValue });
+    const newValue = $event.target.value
+    this.setState({ email: newValue })
   }
 
   changePassword($event) {
-    const newValue = $event.target.value;
-    this.setState({ password: newValue });
+    const newValue = $event.target.value
+    this.setState({ password: newValue })
   }
 
   enviar($event) {
-    $event.preventDefault();
+    $event.preventDefault()
 
-    if (this.state.email !== '' && this.state.password !== '') {
-      let message = JSON.stringify(this.state);
-      fetch('http://localhost:8080/login', {
-        method: 'POST',
-        mode: 'cors',
+    if (this.state.email !== "" && this.state.password !== "") {
+      let message = JSON.stringify(this.state)
+      fetch("http://localhost:8080/login", {
+        method: "POST",
+        mode: "cors",
         body: message,
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        referrerPolicy: 'no-referrer',
+        referrerPolicy: "no-referrer",
       })
         .then((res) => res.json())
         .then((data) => {
-          console.log(data);
-          if (data.msg === 'Login Succes') {
+          console.log(data)
+          if (data.msg === "Login Succes") {
             MySwal.fire({
-              icon: 'success',
-              title: 'Registro',
+              icon: "success",
+              title: "Registro",
               text: data.msg,
-              footer: 'ok',
-            });
+              footer: "ok",
+            })
             // eslint-disable-next-line no-restricted-globals
-            window.location = '/';
+            window.location = "/"
+            PrivateRoute.auth.token = true
           } else {
             MySwal.fire({
-              icon: 'warning',
-              title: 'Ups...',
+              icon: "warning",
+              title: "Ups...",
               text: data.msg,
-              footer: 'Revise que los campos esten bien',
-            });
+              footer: "Revise que los campos esten bien",
+            })
           }
-        });
+        })
     } else {
       MySwal.fire({
-        icon: 'warning',
-        title: 'Ups...',
-        text: 'Parece que olvido llenar todos los campos',
-        footer: 'Por favor llene todos los campos',
-      });
+        icon: "warning",
+        title: "Ups...",
+        text: "Parece que olvido llenar todos los campos",
+        footer: "Por favor llene todos los campos",
+      })
     }
   }
 
@@ -101,13 +103,11 @@ class Login extends Component {
           </div>
           <div className="Enter">
             <button type="button" id="Entrada" onClick={this.enviar}>
-              Iniciar Sesión{' '}
+              Iniciar Sesión{" "}
             </button>
           </div>
           <div className="Nuevo">
             <Link to="/signup">¿Aun no estas registrado?</Link>
-            <br />
-            <Link to="/registrar-organizaciones">Registrar organizacion</Link>
           </div>
           <div className="Olvido">
             <p>¿Olvidaste tu contraseña?</p>
@@ -117,8 +117,8 @@ class Login extends Component {
           <h1>UVGente</h1>
         </div>
       </form>
-    );
+    )
   }
 }
 
-export default Login;
+export default Login
