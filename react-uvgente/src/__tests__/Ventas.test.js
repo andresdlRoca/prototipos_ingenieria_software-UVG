a643b1079841f432c1d58dbbf44a59f8049124fe
andresdlRoca <andresdelaroca11@gmail.com>
2022-09-24 15:21:56 -0600

    Ventas testing

--- react-uvgente/src/__tests__/Ventas.test.js
+++ react-uvgente/src/__tests__/Ventas.test.js
@@ -1,12 +1,23 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
 import React from "react"
 import Ventas from '../components/Ventas/NewVentas';
-import CartasVentas from "../components/Ventas/CardVenta";
+import AgregarProducto from "../components/Mis_Ventas/AgregarProducto";
+import CardsVentas from "../components/Mis_Ventas/CardsVentas";
+import FloatingAgregar from "../components/Mis_Ventas/FloatingAgregar";
+import EditarProductos from "../components/Mis_Ventas/EditarProducto";
 
 describe("Rendering pagina de ventas" , ()=> {
 
   it("Render Ventas", ()=> {
     render(<Ventas />);
+  })
+
+  it("Render Mis Ventas", ()=> {
+    render(<AgregarProducto />)
+  })
+
+  it("Render Pagina de agregar producto", () => {
+    render(<EditarProductos />)
   })
 
 })
