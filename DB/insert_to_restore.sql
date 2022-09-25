f21497b9b0de7d3c1f3516435980b8ad07c55c32
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-09-25 17:25:50 -0600

    Create insert_to_restore.sql

---
+++ DB/insert_to_restore.sql
@@ -0,0 +1,6 @@
+INSERT INTO vendedor(id_usuario, calificacion)VALUES(4, 5);
+INSERT INTO vendedor(id_usuario, calificacion)VALUES(6, 5)
+
+INSERT INTO producto (nombre, src_img, descripcion, calificacion, precio, id_vendedor) 
+VALUES ('Raspberry', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Raspberry_Pi_4_Model_B_-_Side.jpg/1200px-Raspberry_Pi_4_Model_B_-_Side.jpg',
+	   'Raspberry nueva, con poco uso', 4, 100, 1);
