9e113ece8b6940f6df9ca9384479e7909670cf2d
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-09-25 19:18:36 -0600

    Update new_tables_nw_features.sql

--- DB/new_tables_nw_features.sql
+++ DB/new_tables_nw_features.sql
@@ -2,14 +2,14 @@ CREATE TABLE Insignia(
     id_insignia BIGSERIAL PRIMARY KEY, 
     nombre VARCHAR(44), 
     descripcion TEXT, 
-    rareza_en_pordentaje NUMBER,
+    rareza_en_porcentaje NUMERIC,
     tipo VARCHAR(44), 
     nivel VARCHAR(44), 
     imagen TEXT 
 );  
 CREATE TABLE Organizacion(
     id_organizacion BIGSERIAL PRIMARY KEY, 
-    userName VARCHAR(100), 
+    user_name VARCHAR(100), 
     id_usuario_lider BIGINT, 
     password TEXT, 
     email VARCHAR(200), 
@@ -19,17 +19,20 @@ CREATE TABLE Organizacion(
     linkedin TEXT, 
     descripcion TEXT, 
     isActive BOOLEAN, 
-    FOREIGN KEY (id_usuario_lider) REFERENCES Usuario(id), 
+    FOREIGN KEY (id_usuario_lider) REFERENCES Usuario(id)
 );
 CREATE TABLE Organizacion_Colaborador(
     id_relacion_organizacion_colaborador BIGSERIAL PRIMARY KEY, 
     id_organizacion BIGINT, 
     id_usuario BIGINT, 
     rol VARCHAR(100),
-    FOREIGN KEY (id_organizacion) REFERENCES cobro(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_organizacion) REFERENCES Organizacion(id_organizacion) ON DELETE CASCADE,
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id) ON DELETE CASCADE
 );
 CREATE TABLE Insgnia_Organizacion(
     id_relacion_organzacion_insignia BIGSERIAL PRIMARY KEY, 
-    id_ins
-
+    id_insignia BIGINT, 
+    id_organizacion BIGINT, 
+    FOREIGN KEY (id_insignia) REFERENCES Insignia(id_insignia) ON DELETE CASCADE, 
+    FOREIGN KEY (id_organizacion) REFERENCES Organizacion(id_organizacion) ON DELETE CASCADE
 );
