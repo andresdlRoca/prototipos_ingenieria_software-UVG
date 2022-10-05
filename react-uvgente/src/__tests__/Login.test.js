63252b38e1a407b85359a26243ed1a31d0d9f769
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2022-10-05 12:12:50 -0600

    New Tests

--- react-uvgente/src/__tests__/Login.test.js
+++ react-uvgente/src/__tests__/Login.test.js
@@ -1,18 +1,65 @@
-/* eslint-disable no-undef */
-/**
- * @jest-environment jsdom
- */
+import '@testing-library/jest-dom';
+import {
+  getByPlaceholderText,
+  getByText,
+  render,
+  fireEvent,
+  screen,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Login from '../components/Login/Login';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import router from '../../../node-api-postgres/routes/api/functions.js';
+import pool from '../../../node-api-postgres/db-pg-config.js';
 
- import { render, screen } from "@testing-library/react"
- import React from "react"
- import Login from '../components/Login/Login';
- 
- describe("Rendering landing page" , ()=> {
- 
-   it("Render Login", ()=> {
-     render(<Login />);
- 
-   })
- })
- 
-  
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () =>
+      Promise.resolve({ msg: 'Error interno en la busqueda del usuario' }),
+  })
+);
+
+beforeEach(() => {
+  fetch.mockClear();
+});
+
+test('renders the Login page', () => {
+  render(<Login />);
+});
+
+test('Change Email', () => {
+  render(<Login />);
+  const test = screen.getByPlaceholderText('name@example.com');
+  expect(test.value).toBe('');
+
+  userEvent.type(screen.getByPlaceholderText('name@example.com'), 'rober');
+  expect(screen.getByPlaceholderText('name@example.com').value).toBe('rober');
+});
+
+test('Change Password', () => {
+  render(<Login />);
+  const test1 = screen.getByPlaceholderText('Contraseña');
+  expect(test1.value).toBe('');
+
+  userEvent.type(screen.getByPlaceholderText('Contraseña'), 'rober');
+  expect(screen.getByPlaceholderText('Contraseña').value).toBe('rober');
+});
+
+test('Miss2', () => {
+  render(<Login />);
+  const colorButton = screen.getByTestId('Entrada');
+  userEvent.type(screen.getByPlaceholderText('Contraseña'), 'roberto');
+  expect(screen.getByPlaceholderText('Contraseña').value).toBe('roberto');
+  userEvent.type(screen.getByPlaceholderText('name@example.com'), 'roberto');
+  expect(screen.getByPlaceholderText('name@example.com').value).toBe('roberto');
+  fireEvent.click(colorButton);
+});
+
+test('Miss', () => {
+  render(<Login />);
+  const colorButton = screen.getByTestId('Entrada');
+  expect(screen.getByPlaceholderText('Contraseña').value).toBe('');
+  fireEvent.click(colorButton);
+  expect(screen.getByText('Ups...').innerHTML).toBe('Ups...');
+});
