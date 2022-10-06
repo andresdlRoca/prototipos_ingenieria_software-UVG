6de8de530980be13c3875832e881c49ae1684eeb
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2022-10-05 19:15:18 -0600

    Test-Friendly

--- react-uvgente/src/components/new-side-bar/NewSideBar.js
+++ react-uvgente/src/components/new-side-bar/NewSideBar.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import './side-bar-style.css';
-import Media from 'react-media';
-
 import {
   FaInbox,
   FaCog,
@@ -12,70 +10,62 @@ import {
   FaBriefcase,
 } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-
 const NewSideBar = () => {
   const [isHidden, setIsHidden] = useState(false);
   return (
-    <div>
-      <Media query={'(min-width: 600px)'}>
-        {(matches) => {
-          return matches ? (
-            <div
-              id={
-                isHidden
-                  ? 'main-wrapper-side-bar-shown'
-                  : 'main-wrapper-side-bar-hidden'
-              }
-              onClick={() => setIsHidden(!isHidden)}
-            >
-              <div id="space-for-star-ratting-side-bar">
-                <div className="star-rate-on-side-bar" />
-                <div className="star-rate-on-side-bar" />
-                <div className="star-rate-on-side-bar" />
-                <div className="star-rate-on-side-bar" />
-                <div className="star-rate-on-side-bar" />
-              </div>
-              <ul id="side-bar-list">
-                <li className="item-side-bar-container">
-                  <Link className="item-on-side-bar" to="/bandeja-de-entrada">
-                    <p>Bandeja de entrada</p>
-                    <FaInbox className="icon-on-item-side-bar" />
-                  </Link>
-                </li>
-                <li className="item-side-bar-container">
-                  <Link className="item-on-side-bar" to="/mis-compras">
-                    <p>Mis compras</p>
-                    <FaShoppingBag className="icon-on-item-side-bar" />
-                  </Link>
-                </li>
-                <li className="item-side-bar-container">
-                  <Link className="item-on-side-bar" to="/articulos-publicados">
-                    <p>Artículos publicados</p>
-                    <FaBriefcase className="icon-on-item-side-bar" />
-                  </Link>
-                </li>
-                <li className="item-side-bar-container">
-                  <Link className="item-on-side-bar" to="/favorites">
-                    <p>Guardados</p>
-                    <FaBookmark className="icon-on-item-side-bar" />
-                  </Link>
-                </li>
-              </ul>
-              <div id="app-ajustment-options">
-                <Link to="/ajustes" className="new-link">
-                  <FaCog />{' '}
-                </Link>
-                <Link to="/ayuda" className="new-link">
-                  <FaQuestion />{' '}
-                </Link>
-                <Link to="/report" className="new-link">
-                  <FaExclamationTriangle />
-                </Link>
-              </div>
-            </div>
-          ) : null;
-        }}
-      </Media>
+    <div
+      data-testid="sidebar"
+      id={
+        isHidden
+          ? 'main-wrapper-side-bar-shown'
+          : 'main-wrapper-side-bar-hidden'
+      }
+      onClick={() => setIsHidden(!isHidden)}
+    >
+      <div id="space-for-star-ratting-side-bar">
+        <div className="star-rate-on-side-bar" />
+        <div className="star-rate-on-side-bar" />
+        <div className="star-rate-on-side-bar" />
+        <div className="star-rate-on-side-bar" />
+        <div className="star-rate-on-side-bar" />
+      </div>
+      <ul id="side-bar-list">
+        <li className="item-side-bar-container">
+          <Link className="item-on-side-bar" to="/bandeja-de-entrada">
+            <p>Bandeja de entrada</p>
+            <FaInbox className="icon-on-item-side-bar" />
+          </Link>
+        </li>
+        <li className="item-side-bar-container">
+          <Link className="item-on-side-bar" to="/mis-compras">
+            <p>Mis compras</p>
+            <FaShoppingBag className="icon-on-item-side-bar" />
+          </Link>
+        </li>
+        <li className="item-side-bar-container">
+          <Link className="item-on-side-bar" to="/articulos-publicados">
+            <p>Artículos publicados</p>
+            <FaBriefcase className="icon-on-item-side-bar" />
+          </Link>
+        </li>
+        <li className="item-side-bar-container">
+          <Link className="item-on-side-bar" to="/favorites">
+            <p>Guardados</p>
+            <FaBookmark className="icon-on-item-side-bar" />
+          </Link>
+        </li>
+      </ul>
+      <div id="app-ajustment-options">
+        <Link to="/ajustes" className="new-link">
+          <FaCog />{' '}
+        </Link>
+        <Link to="/ayuda" className="new-link">
+          <FaQuestion />{' '}
+        </Link>
+        <Link to="/report" className="new-link">
+          <FaExclamationTriangle />
+        </Link>
+      </div>
     </div>
   );
 };
