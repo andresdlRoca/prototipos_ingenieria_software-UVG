b29569510ab6d9c449a892261579973fa2e51d51
robertriosm <87707371+robertriosm@users.noreply.github.com>
2022-10-08 17:10:13 -0600

    avances tests

--- react-uvgente/src/__tests__/Mainpage.test.js
+++ react-uvgente/src/__tests__/Mainpage.test.js
@@ -2,6 +2,16 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import NewPaginaPrincipal from '../components/NewPaginaPrincipal/NewPaginaPrincipal';
 
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
+
 test('renders the pagina principal', () => {
   render(<NewPaginaPrincipal />);
 });
