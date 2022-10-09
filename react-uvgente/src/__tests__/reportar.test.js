1afe45998623ef2331d80174b166f6523d2ad553
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2022-10-08 23:24:53 -0600

    Add files via upload

--- react-uvgente/src/__tests__/reportar.test.js
+++ react-uvgente/src/__tests__/reportar.test.js
@@ -1,8 +1,70 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import ReportarProblema from '../components/ReportarProblema/ReportarProblema';
-import React from "react"
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import ReportarProblema from '../components/ReportarProblema/ReportarProblema.js';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
 
-test('renders the Mi perfil page', () => {
+test('renders the Reportar Problema page', () => {
   render(<ReportarProblema />);
+});
+
+test('renders  send', () => {
+  render(<ReportarProblema />);
+  const test = screen.getByTestId('Enviar');
+  fireEvent.click(test);
+});
+
+test('renders  recieve', () => {
+  render(<ReportarProblema />);
+  const test = screen.getByTestId('Enviar');
+  userEvent.type(
+    screen.getByPlaceholderText(
+      'Por favor incluir la mayor información posible'
+    ),
+    'rober'
+  );
+  fireEvent.click(test);
+});
+
+test('Hit', () => {
+  render(<ReportarProblema />);
+  global.fetch = jest.fn().mockImplementation(() => {
+    return new Promise((resolve) =>
+      resolve({
+        json: () => {
+          return { status: 'Login Succes' };
+        },
+      })
+    );
+  });
+  const test = screen.getByTestId('Enviar');
+  userEvent.type(
+    screen.getByPlaceholderText(
+      'Por favor incluir la mayor información posible'
+    ),
+    'rober'
+  );
+  fireEvent.click(test);
+});
+
+test('Miss', () => {
+  global.fetch = jest.fn().mockImplementation(() => {
+    return new Promise((resolve) =>
+      resolve({
+        json: () => {
+          return { status: '200' };
+        },
+      })
+    );
+  });
+  render(<ReportarProblema />);
+  const test = screen.getByTestId('Enviar');
+  userEvent.type(
+    screen.getByPlaceholderText(
+      'Por favor incluir la mayor información posible'
+    ),
+    'rober'
+  );
+  fireEvent.click(test);
 });
