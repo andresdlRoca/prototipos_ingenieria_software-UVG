b13d4a64092aab5bb1f7576034a7594b86026f18
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-10-08 22:55:28 -0600

    Update functions.js
    - Reomoved duplicated endpoint vender
    - Add endpoints for testing
    - Fix register organization
    - Minor changes on user register

--- node-api-postgres/routes/api/functions.js
+++ node-api-postgres/routes/api/functions.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 const secretKey = 'wowMissawow';
 const saltRounds = 10;
 const myPool = (NODE_ENV== 'test ' || NODE_ENV== 'development ')? pool_test: pool
-
+//Tests done
 router.post('/register', (req, response) => {
   let { username, email, password } = req.body;
   if (!(username && email && password))
@@ -17,7 +17,7 @@ router.post('/register', (req, response) => {
   bcrypt.hash(password, saltRounds, (err, hash) => {
     if (err) return response.status(500).json({ msg: 'DB error ocurred', err });
     myPool.query(
-      'INSERT INTO usuario (username,email, password) VALUES ($1, $2, $3)',
+      'INSERT INTO usuario (username,email, password) VALUES ($1, $2, $3) RETURNING id',
       [username, email, hash],
       (error, result) => {
         if (error)
@@ -25,56 +25,10 @@ router.post('/register', (req, response) => {
             return response
             .status(500)
             .json({  msg: "Error on query",  error});}
-        return response.status(201).json({msg: 'Registro realizado con exito'});
+        return response.status(201).json({msg: 'Registro realizado con exito', result: result.rows[0]});
       }
     );
   });
-});
-
-router.post('/vender', (req, res) => {
-  let { nombre, descripcion, precio, form } = req.body;
-  if (precio && nombre && descripcion) {
-    if (form == 'producto') {
-      myPool.query(
-        'INSERT INTO Producto (nombre, precio, disponible, descripcion, id_vendedor) VALUES ($1, $2, true, $3, 2) RETURNING *',
-        [nombre, precio, descripcion],
-        (error, results) => {
-          if (error) {
-            return res.status(500).json({
-              msg: error,
-              error,
-            });
-            {
-              msg: 'El usuario fue registrado correctamente!!';
-            }
-          }
-          return res.status(201).json({
-            msg: 'El producto fue registrado con exito',
-            product_id: results.rows.id,
-          });
-        }
-      );
-    } else {
-      // myPool.query(
-      //   "INSERT INTO Producto (nombre, precio, disponible, descripcion, id_vendedor) VALUES ($1, $2, true, $3, 2) RETURNING *",
-      //   [nombre, precio, descripcion],
-      //   (error, results) => {
-      //     if (error) {
-      //       return res.status(500).json({
-      //         msg: error,
-      //         error,
-      //       });
-      //     }
-      //     return res.status(201).json({
-      //       msg: "El producto fue registrado con exito",
-      //       product_id: results.rows.id,
-      //     });
-      //   });
-    }
-  } else
-    return res
-      .status(404)
-      .json({ msg: 'No se completaron campos suficientes' });
 });
 
 router.post('/new-product', (req, res) => {
@@ -97,43 +51,27 @@ router.post('/new-product', (req, res) => {
       }
     );
   } else
-    return res
-      .status(404)
-      .json({ msg: 'No se completaron campos suficientes' });
+    return res.status(404).json({ msg: 'No se completaron campos suficientes' });
 });
 
 router.post('/registrar-organizaciones', (req, res) => {
-  let { nombre, passw, correo, correoA, username, telefono, imagen } = req.body;
-  if (nombre && passw && correo && correoA && username && telefono && imagen) {
+  let { username, password, email, descripcion, no_telefono } = req.body;
+  if (!(username && password && email && descripcion && no_telefono )) 
+  return res.status(404).json({ msg: 'Hubo un error, revise los campos' });
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) return res.status(500).json({msg: "An unexpected error ocurred while hashing password"})
     myPool.query(
-      'INSERT INTO organizacion (id_organizacion, username, password, email, alternative_email, no_telefono, profile_pic, nombre, apellido, linkedin, descripcion, isActive) VALUES (0, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *',
-      [
-        username,
-        passw,
-        correo,
-        correoA,
-        telefono,
-        imagen,
-        nombre,
-        '-',
-        '-',
-        '-',
-        true,
-      ],
+      'INSERT INTO organizacion (user_name, password, email, no_telefono, descripcion, isActive) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id_organizacion ',
+      [username, hash, email, no_telefono, descripcion, true],
       (error, results) => {
-        if (error)
-          return res.status(500).json({
-            msg: 'Algo salio mal, no se pudo registrar la organizacion',
-            error,
-          });
-        return res.status(201).json({
-          msg: 'La organizacion registrado con exito',
-          product_id: results.rows.id,
-        });
+        if (error) return res.status(500).json({msg: 'An error occured while making the query',error});
+        return res.status(201).json({msg: 'La organizacion registrado con exito', result: results.rows[0]});
       }
-    );
-  } else
-    return res.status(404).json({ msg: 'Hubo un error, revise los campos' });
+  );
+  })
+
+  
+    
 });
 
 router.get('/class/:id', (req, res) => {
@@ -415,6 +353,22 @@ router.put('/delete-user-by-username/:username', (req, res)=>{
   })
 })
 
+router.put('/create-vendedor-on-organizacion/:id', (req, res)=>{
+  const id = parseInt(req.params.id)
+  myPool.query('INSERT INTO vendedor (id_organizacion) VALUES ($1)', [id], (error, result)=>{
+    if (error) return res.status(500).json({msg: "Error durign query", error})
+    return res.status(200).json({msg: 'Vendedor creado exitosamente'})
+  })
+})
+
+router.put('/create-vendedor-on-user/:id', (req, res)=>{
+  const id = parseInt(req.params.id)
+  myPool.query('INSERT INTO vendedor (id_usuario) VALUES ($1)', [id], (error, result)=>{
+    if (error) return res.status(500).json({msg: "Error durign query", error})
+    return res.status(200).json({msg: 'Vendedor creado exitosamente'})
+  })
+})
+
 router.put('/clean-users-table', (req, res)=>{
   myPool.query('DELETE FROM usuario;', [], (error, results)=>{
     if(error) return res.status(500).json({msg: 'An error happend while making the query.', code: error.code, details: error.details})
@@ -443,5 +397,11 @@ router.put('/clean-bitacora_ventas-table', (req, res)=>{
   })
 })
 
+router.put('/clean-ventas', (req, res)=>{
+  myPool.query('DELETE FROM bitacora_ventas;', [], (error, results)=>{
+    if(error) return res.status(500).json({msg: 'An error happend while making the query.', code: error.code, details: error.details})
+    res.status(200).json({msg: 'Deletion completed', count: results.rowCount})
+  })
+})
 
 module.exports = router;
