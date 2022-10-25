f7c1c1816bdffd23a63b154d7dba93ada9efcac5
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2022-10-24 19:15:15 -0600

    Add files via upload

--- react-uvgente/src/components/Mis_Ventas/AgregarProducto.js
+++ react-uvgente/src/components/Mis_Ventas/AgregarProducto.js
@@ -1,53 +1,75 @@
-import React, { Fragment, useState } from 'react'
-import "./Cards_Ventas.css"
+import React, { Fragment, useState } from 'react';
+import './Cards_Ventas.css';
 
 const AgregarProducto = (props) => {
+  const [productInfo, sumbitProductInfo] = useState({
+    nombre: '',
+    precio: '',
+    categoria: '',
+    descripcion: '',
+  });
 
-    const [productInfo, sumbitProductInfo] = useState({
-        nombre: '',
-        precio: '',
-        categoria: '',
-        descripcion: ''
-    })
+  const handleChange = (event) => {
+    sumbitProductInfo({
+      ...productInfo,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    const handleChange = (event) => {
-        sumbitProductInfo({
-            ...productInfo,
-            [event.target.name] : event.target.value
-        })
-    }
+  const handleSumbit = (event) => {
+    event.preventDefault();
+    console.log('Datos: \nNombre: ' + productInfo.nombre);
+    console.log('Precio: ' + productInfo.precio);
+    console.log('Categoria: ' + productInfo.categoria);
+    console.log('Descripcion: ' + productInfo.descripcion);
+  };
 
-    const handleSumbit = (event) => {
-        event.preventDefault()
-        console.log("Datos: \nNombre: "+ productInfo.nombre)
-        console.log("Precio: " + productInfo.precio)
-        console.log("Categoria: " + productInfo.categoria)
-        console.log("Descripcion: " + productInfo.descripcion)
-    }
+  return (
+    <Fragment>
+      <h1 className="textVender">Vender producto</h1>
+      <form className="formProducto" onSubmit={handleSumbit}>
+        <div className="pregunta">
+          <input
+            type="text"
+            placeholder="Nombre del producto"
+            className="formInput"
+            onChange={handleChange}
+            name="nombre"
+          ></input>
+        </div>
+        <div className="pregunta">
+          <input
+            type="text"
+            placeholder="Precio del producto"
+            className="formInput"
+            onChange={handleChange}
+            name="precio"
+          ></input>
+        </div>
+        <div className="pregunta">
+          <input
+            type="text"
+            placeholder="Categoria"
+            className="formInput"
+            onChange={handleChange}
+            name="categoria"
+          ></input>
+        </div>
+        <div className="pregunta">
+          <input
+            type="text"
+            placeholder="Descripcion"
+            className="formInput"
+            onChange={handleChange}
+            name="descripcion"
+          ></input>
+        </div>
+        <button type="sumbit" className="buttonSumbit" data-testid="Submit">
+          Continuar
+        </button>
+      </form>
+    </Fragment>
+  );
+};
 
-
-    return(
-        <Fragment>
-            <h1 className='textVender'>Vender producto</h1>
-            <form className='formProducto' onSubmit={handleSumbit}>
-                <div className='pregunta'>
-                    <input type="text" placeholder="Nombre del producto" className="formInput" onChange={handleChange} name="nombre"></input>
-                </div>
-                <div className='pregunta'>
-                    <input type="text" placeholder="Precio del producto" className="formInput" onChange={handleChange} name="precio"></input>
-                </div>
-                <div className='pregunta'>
-                    <input type="text" placeholder="Categoria" className="formInput" onChange={handleChange} name="categoria"></input>
-                </div>
-                <div className='pregunta'>
-                    <input type="text" placeholder="Descripcion" className="formInput" onChange={handleChange} name="descripcion"></input>
-                </div>
-                <button type="sumbit" className='buttonSumbit'>Continuar</button>
-            </form>
-        </Fragment>
-        
-    )
-
-}
-
-export default AgregarProducto
+export default AgregarProducto;
