86603561fbd63cded50f59b8e866a3389857619f
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2022-10-24 18:58:20 -0600

    Add files via upload

--- react-uvgente/src/__tests__/MisVentas.test.js
+++ react-uvgente/src/__tests__/MisVentas.test.js
@@ -1,32 +1,43 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import AgregarProducto from '../components/Mis_Ventas/AgregarProducto';
 import CardsVentas from '../components/Mis_Ventas/CardsVentas';
 import EditarProductos from '../components/Mis_Ventas/EditarProducto';
 import FloatingAgregar from '../components/Mis_Ventas/FloatingAgregar';
 import CardVenta from '../components/Mis_Ventas/NewCards_Ventas';
-import React from "react"
+import { MemoryRouter } from 'react-router-dom';
 
-describe("Rendering pagina de ajustes" , ()=> {
+test('renders the AgregarProducto page', () => {
+  render(<AgregarProducto />);
+  const test1 = screen.getByTestId('Submit');
+  userEvent.type(screen.getByPlaceholderText('Nombre del producto'), 'roberto');
+  userEvent.type(screen.getByPlaceholderText('Precio del producto'), '100');
+  userEvent.type(screen.getByPlaceholderText('Categoria'), '100');
+  userEvent.type(screen.getByPlaceholderText('Descripcion'), '100');
+  fireEvent.click(test1);
+});
 
-  it("renders the AgregarProducto page", ()=> {
-    render(<AgregarProducto />);
-  })
+test('renders the CardsVentas page', () => {
+  render(<CardsVentas />, { wrapper: MemoryRouter });
+});
 
-  it('renders the CardsVentas page', () => {
-    render(<CardsVentas title="Articulos" />);
-  })
-  
-  it('renders the EditarProducto page', () => {
-    render(<EditarProductos />);
-  })
-  
-  it('renders the FlaotingAgregar page', () => {
-    render(<FloatingAgregar />);
-  })
-  
-  it('renders the NewCards_Ventas page', () => {
-    render(<CardVenta />);
-  })
+test('renders the EditarProducto page', () => {
+  render(<EditarProductos />);
+  const test1 = screen.getByTestId('Submit');
+  userEvent.type(screen.getByPlaceholderText('Nombre del producto'), 'roberto');
+  userEvent.type(screen.getByPlaceholderText('Precio del producto'), '100');
+  userEvent.type(screen.getByPlaceholderText('Categoria'), '100');
+  userEvent.type(screen.getByPlaceholderText('Descripcion'), '100');
+  fireEvent.click(test1);
+});
 
-})
+test('renders the FlaotingAgregar page', () => {
+  render(<FloatingAgregar />, { wrapper: MemoryRouter });
+  const test1 = screen.getByTestId('Hello');
+  fireEvent.click(test1);
+});
+
+test('renders the NewCards_Ventas page', () => {
+  render(<CardVenta />);
+});
