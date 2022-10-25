05d43015414ebe6292ad8770a83430ece183cb39
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2022-10-24 19:48:01 -0600

    Test Opciones Venta

--- react-uvgente/src/__tests__/opcionesventa.test.js
+++ react-uvgente/src/__tests__/opcionesventa.test.js
@@ -15,4 +15,11 @@ test('renders the opciones_de_venta', () => {
 test('testoption1', () => {
   render(<OpcionesDeVenta />);
   const object1 = screen.getByTestId('Opcion1');
+  const object2 = screen.getByTestId('Opcion2');
+  fireEvent.click(object1);
+  fireEvent.click(object2);
+  const object3 = screen.getByTestId('Opcion3');
+  const object4 = screen.getByTestId('Opcion4');
+  fireEvent.click(object3);
+  fireEvent.click(object4);
 });
