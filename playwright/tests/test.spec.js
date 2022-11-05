e4140aa08d82e719c2de7a0855a74312ac6a2d14
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-11-05 00:11:15 -0600

    First e2e tests: signup

---
+++ playwright/tests/test.spec.js
@@ -0,0 +1,62 @@
+import { test, expect } from '@playwright/test';
+const Chance = require('chance')
+const chance = new Chance()
+test.describe("Signups",()=>{
+
+  test.beforeEach(async ({ page, request }) => {
+    // Go to the starting url before each test.
+    await page.goto("http://localhost:3000");
+    await request.put('http://localhost:8080/clean-users-table')
+  });
+  test.afterAll(async({request})=>{
+    await request.put('http://localhost:8080/clean-users-table')
+  })
+  test('Usual Signup and redirect', async ({ page }) => {
+
+    await page.getByRole('link', { name: 'Log in' }).click();
+    await expect(page).toHaveURL('http://localhost:3000/login');
+
+    await page.getByRole('link', { name: 'Signup' }).click();
+    await expect(page).toHaveURL('http://localhost:3000/signup');
+
+    await page.getByTestId('username').click();
+
+    await page.getByTestId('username').fill(chance.name());
+
+    await page.getByTestId('username').press('Tab');
+
+    await page.getByTestId('email').fill(chance.email());
+
+    await page.getByTestId('email').press('Tab');
+
+    await page.getByTestId('password').fill('password');
+
+    await page.getByTestId('password').press('Tab');
+
+    await page.getByTestId('confirm-password').fill('password');
+
+    await page.getByTestId('Enviar').click();
+
+    await page.waitForURL('http://localhost:3000/')
+
+    //await expect(page).toHaveProperty()
+
+  });
+  test('Failed signup: Missing fields', async ({ page }) => {
+    await page.getByRole('link', { name: 'Log in' }).click();
+    await expect(page).toHaveURL('http://localhost:3000/login');
+    await page.getByRole('link', { name: 'Signup' }).click();
+    await expect(page).toHaveURL('http://localhost:3000/signup');
+    await page.getByTestId('email').click();
+    await page.getByTestId('email').fill(chance.email());
+    await page.getByTestId('email').press('Tab');
+    await page.getByTestId('password').fill('password');
+    await page.getByTestId('password').press('Tab');
+    await page.getByTestId('confirm-password').fill('password');
+    await page.getByTestId('Enviar').click();
+    await expect(page.locator('#swal2-html-container')).toHaveText('Parece que olvido llenar todos los campos')
+    await page.getByRole('button', { name: 'OK' }).click();
+  });
+
+
+})
