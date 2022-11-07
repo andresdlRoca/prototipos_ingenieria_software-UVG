0fa97250de3f2af1f6e831f5e682e38c3f334ecf
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2022-11-07 13:56:16 -0600

    Add files via upload

--- react-uvgente/src/__tests__/navbar.test.js
+++ react-uvgente/src/__tests__/navbar.test.js
@@ -1,9 +1,18 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 // import MiPerfil from '../components/MiPerfil/MiPerfil';
 import NavBar from '../components/new-nav-bar/NavBar';
 import NewNavBarItem from '../components/new-nav-bar/NewNavBarItem';
-import React from "react"
+import MobileNavBar from '../components/new-nav-bar/MobileNavBar';
+import React from 'react';
+import mediaQuery from 'css-mediaquery';
+import { createShallow, createMount } from '@material-ui/core/test-utils';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import IconButton from '@mui/material/IconButton';
+import { MemoryRouter } from 'react-router-dom';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 window.matchMedia =
   window.matchMedia ||
@@ -15,18 +24,70 @@ window.matchMedia =
     };
   };
 
+describe('<MyComponent />', () => {
+  it('should work', () => {
+    const view = render(<MobileNavBar />, { wrapper: MemoryRouter });
+    console.log(view);
+
+    const submitButton = screen.getByTestId('OpenNav');
+    fireEvent.click(submitButton);
+  });
+});
+
+describe('<MyComponent/>', () => {
+  it('works', () => {
+    const view = render(<MobileNavBar />, { wrapper: MemoryRouter });
+    console.log(view);
+
+    const submitButton = screen.getByTestId('OpenMenu');
+    fireEvent.click(submitButton);
+  });
+});
+
+describe('<MyComponent/', () => {
+  it('working', () => {
+    const view = render(<MobileNavBar />, { wrapper: MemoryRouter });
+    console.log(view);
+
+    const submitButton = screen.getByTestId('Ajustes');
+    fireEvent.click(submitButton);
+  });
+});
+
+describe('<MyComponen/', () => {
+  it('working', () => {
+    const view = render(<MobileNavBar />, { wrapper: MemoryRouter });
+    console.log(view);
+
+    const submitButton = screen.getByTestId('Mi perfil');
+    fireEvent.click(submitButton);
+  });
+});
+
+const createMatchMedia = (width) => (query) => ({
+  matches: mediaQuery.match(query, { width }),
+  addListener: () => {},
+  removeListener: () => {},
+});
+
 test('renders the Navbar', () => {
+  window.matchMedia = createMatchMedia(600);
+
   render(<NavBar />);
 });
 
-describe("render navbar test", () => {
+test('renders the Navbar mobile', () => {
+  window.matchMedia = createMatchMedia(600);
+  render(<MobileNavBar />);
+  fireEvent.click(screen.getByTestId('OpenNav'));
+});
 
+describe('render navbar test', () => {
   it('renders the Navbar', () => {
     render(<NavBar />);
-  })
-  
+  });
+
   it('renders the newnavbaritem', () => {
     render(<NewNavBarItem />);
-  })
-
-})
+  });
+});
