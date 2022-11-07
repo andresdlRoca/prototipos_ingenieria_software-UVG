276f722accf998e1cdfdcc1788cf1d057812c3a0
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2022-11-06 21:33:41 -0600

    Add files via upload

--- react-uvgente/src/components/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom"
-
-var PrivateRoute = ({ children, ...rest }) => {
-  let auth = { token: true }
-  return auth.token ? <Outlet /> : <Navigate to="/login" />
-}
-
-export default PrivateRoute
