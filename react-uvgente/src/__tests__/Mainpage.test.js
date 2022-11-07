23416724a6431c1a136ee4b9b1d896901c106fb6
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2022-11-07 14:02:29 -0600

    Add files via upload

--- react-uvgente/src/__tests__/Mainpage.test.js
+++ react-uvgente/src/__tests__/Mainpage.test.js
@@ -1,7 +1,14 @@
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import NewPaginaPrincipal from '../components/NewPaginaPrincipal/NewPaginaPrincipal';
+import axios, { AxiosResponse } from 'axios';
+import { act, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
+import NewPaginaPrincipal from '../components/NewPaginaPrincipal/NewPaginaPrincipal';
+import { MemoryRouter } from 'react-router-dom';
+import { renderHook } from '@testing-library/react-hooks';
+import { resolveComponentProps } from '@mui/base';
+import fetchMock from 'jest-fetch-mock';
+
+fetchMock.enableMocks();
 
 window.matchMedia =
   window.matchMedia ||
@@ -14,5 +21,42 @@ window.matchMedia =
   };
 
 test('renders the pagina principal', () => {
-  render(<NewPaginaPrincipal />);
+  global.fetch = jest.fn().mockImplementation(() => {
+    return new Promise((resolve) =>
+      resolve({
+        json: () => {
+          return [
+            {
+              title: 'a',
+              description: 'b',
+              prod_rate: 1,
+              price: 100,
+              src_img: 'a',
+              name: 'c',
+            },
+            {
+              title: 'a',
+              description: 'b',
+              prod_rate: 1,
+              price: 100,
+              src_img:
+                'https://megamitensei.fandom.com/wiki/Path_of_Da%27at?file=Mementos_Dungeon_Art_P5R_2.png',
+              name: 'c',
+              onTopVentas: true,
+            },
+          ];
+        },
+      })
+    );
+  });
+  render(<NewPaginaPrincipal />, { wrapper: MemoryRouter });
+});
+
+test('rendersthe pagina principal', () => {
+  global.fetch = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      reject('error');
+    });
+  });
+  render(<NewPaginaPrincipal />, { wrapper: MemoryRouter });
 });
