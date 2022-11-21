4d1a66c57ff8c1c2b1456505b7eb1d9429431a18
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2022-11-20 22:26:08 -0600

    Delete react-uvgente/src/__tests__/__tests__ directory

--- react-uvgente/src/__tests__/__tests__/Favoritos.test.js
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import Favoritos from '../components/Favoritos/Favorites';
-import React from "react"
-
-test('renders the Favoritos page', () => {
-  render(<Favoritos />);
-});
